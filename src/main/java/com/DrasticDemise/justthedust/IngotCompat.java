0a2843681699071776e7c877eb364731c1e5b6cb
Bailey Hollingsworth <baileyholl@gmail.com>
2016-03-02 15:15:39 -0600

    Revert "Revert "Revert "I broke something"""
    This reverts commit 8411a0ed3ad1204896e506f2db180622d8ea8727.

---
+++ src/main/java/com/DrasticDemise/justthedust/IngotCompat.java
@@ -0,0 +1,21 @@
+package com.DrasticDemise.justthedust;
+
+import java.util.ArrayList;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class IngotCompat {
+	
+	public static ItemStack copperFinder(){
+			return OreDictionary.getOres("ingotCopper").get(0);
+	}
+	public static ItemStack silverFinder(){
+		return OreDictionary.getOres("ingotSilver").get(0);
+	}
+	public static ItemStack leadFinder(){
+		return OreDictionary.getOres("ingotLead").get(0);
+	}
+	public static ItemStack tinFinder(){
+		return OreDictionary.getOres("ingotTin").get(0);
+	}
+}
---
+++ src/main/java/items/GoldDust.java
@@ -0,0 +1,22 @@
+package items;
+
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class GoldDust extends Item{
+		public GoldDust(){
+			setRegistryName("goldDust");
+			setUnlocalizedName("goldDust");
+			GameRegistry.registerItem(this);
+			setCreativeTab(CreativeTabs.tabMaterials);
+		}
+		@SideOnly(Side.CLIENT)
+	    public void initModel() {
+	        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+	    }
+}
